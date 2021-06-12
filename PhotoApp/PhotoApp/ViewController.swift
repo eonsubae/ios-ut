cd50c06467b2b471e81a32b0d22752aff3609af9
eonsubae <eonsubae@naver.com>
2021-06-12 10:24:23 +0900

    Enable Unit Tests in Xcode Project

---
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by KRENGLSSEAN on 2021/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
